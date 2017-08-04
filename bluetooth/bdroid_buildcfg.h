33ffff2f00fbe98090685e0e6137a94cdbfa368c
Nicholas Flintham <nick@flinny.org>
2017-08-04 10:27:03 +0100

    ocn: update bluetooth build config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,10 +18,12 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "HTC U11"
-
-#define BLE_VND_INCLUDED                TRUE
-#define BLUETOOTH_QTI_SW                TRUE
-#define BTM_WBS_INCLUDED                FALSE
-
+#define BTM_DEF_LOCAL_NAME "HTC U11"
+#define BLUETOOTH_QTI_SW TRUE
+// Disables read remote device feature
+#define MAX_ACL_CONNECTIONS   16
+#define MAX_L2CAP_CHANNELS    16
+#define BLE_VND_INCLUDED   TRUE
+// skips conn update at conn completion
+#define BT_CLEAN_TURN_ON_DISABLED 1
 #endif
