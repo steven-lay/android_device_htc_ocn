a29a5a9f14dae00b326c74069a5083ad1bf88857
Nicholas Flintham <nick@flinny.org>
2017-11-20 19:52:41 +0000

    ocn: update config.fs from caf
    Change-Id: I8cdc1903f0fa6d33d2edc2185de7b9abcea33d2a

--- device.mk
+++ device.mk
@@ -356,6 +356,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# For android_filesystem_config.h
-PRODUCT_PACKAGES += fs_config_files \
-                    fs_config_dirs
