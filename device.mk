d40742570a48285934234843c5c0015429f26f93
Michael Bestas <mkbestas@lineageos.org>
2018-01-25 23:13:40 +0200

    msm8998-common: Do not build audiod
    * It's now deprecated
    
    Change-Id: I00c8bdc09da4ba39508314ec8feba67d60e4fb04

--- device.mk
+++ device.mk
@@ -99,7 +99,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    audiod \
     audio.a2dp.default \
     audio.primary.msm8998 \
     audio.r_submix.default \
