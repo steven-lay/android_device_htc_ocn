70c0268f5422559924c4c7cb75a97741b34d18d9
Nicholas Flintham <nick@flinny.org>
2018-02-13 21:07:36 +0000

    ocn: initial sepolicy for O
    Change-Id: I21f135ea53c62871eb09763bad36152edf789395

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,7 +196,7 @@ PROTOBUF_SUPPORTED := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
