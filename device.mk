efc7503567bacb8417a3a025d88b02eaa8ebe78f
Nicholas Flintham <nick@flinny.org>
2018-02-14 13:33:13 +0000

    ocn: add sound_trigger and update audio build flags
    Change-Id: Ia371ac34eb8b1b714948de9cb07cc3998315ad5a

--- device.mk
+++ device.mk
@@ -96,6 +96,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8998 \
