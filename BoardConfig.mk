7e9936944f11dba2928dc2fc852a78ded9c21d12
Nicholas Flintham <nick@flinny.org>
2018-02-15 21:11:42 +0000

    ocn: more sepolicy
    Change-Id: I875efbac26b5d7b593186e8795c82af39e35465c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,7 +195,7 @@ TARGET_RIL_VARIANT := caf
 PROTOBUF_SUPPORTED := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+-include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Sensors
