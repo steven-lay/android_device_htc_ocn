1b3e0e5fa84d253dbaea91ab282b981f570c7e07
Nicholas Flintham <nick@flinny.org>
2018-05-08 17:02:17 +0100

    ocn: Enable TARGET_USES_QTI_CAMERA_DEVICE
    Change-Id: I92f70eb2ce3951774bdde13186f7a308d87f95d3

--- device.mk
+++ device.mk
@@ -138,6 +138,7 @@ PRODUCT_PACKAGES += \
  PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     libqomx_core \
     libmmjpeg_interface \
