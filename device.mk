937b72b009d65348fba844f25a479910e478b880
Daniel Gomez <aka.mdsg@gmail.com>
2018-05-21 02:19:23 +0000

    ocn: audio: disable soundtrigger to fix hotword detection

--- device.mk
+++ device.mk
@@ -96,7 +96,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8998 \
