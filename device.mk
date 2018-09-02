dc01ec9e40ef5b291ac6b4cd9110eb91aa39ec2a
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-02 12:21:32 -0600

    ocn: Build a system version of android.hidl.base@1.0 for imsvt
    Change-Id: I5aa2e747b6ec4138c9c135c34a1975016e663b79
    (cherry picked from commit e4a19a8f1c20ff9799c2660fd456cb5ec8ffe064)

--- device.mk
+++ device.mk
@@ -218,6 +218,7 @@ PRODUCT_PACKAGES += \
 # hidl base, or8 props still depend on it
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
