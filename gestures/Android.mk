0392adaaaaad255c5b7694ea2d6adf1e732628ad
tomascus <arbiter1000@gmail.com>
2018-09-16 12:45:08 +1000

    ocn: gestures: set sdk version
    Change-Id: I00bb7983198d68ba373af432a7f2bb02a657b2ff

--- gestures/Android.mk
+++ gestures/Android.mk
@@ -27,6 +27,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := GestureHandler
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
