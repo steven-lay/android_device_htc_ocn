01019128c2396081b1e2d642e9fade8967f67ccc
tomascus <arbiter1000@gmail.com>
2018-09-16 12:40:51 +1000

    DNM: remove camera provider
    Change-Id: I20de6406cbe2f7ba5b2188172a75d1b19d1e2802

--- device.mk
+++ device.mk
@@ -135,8 +135,6 @@ PRODUCT_PACKAGES += \
 
 # Camera 
  PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     libqomx_core \
