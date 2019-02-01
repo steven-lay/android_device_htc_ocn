7b8494dec7fd7c611f5e06b99a7eacd402324a77
tomascus <arbiter1000@gmail.com>
2019-02-02 10:10:03 +1100

    ocn: build libims_shim as 64 bit
    The library shimmed against is also 64 bit
    
    Change-Id: Ibe490e5e901a8022365ac9ba1382a423e418234d

--- shims/Android.mk
+++ shims/Android.mk
@@ -9,7 +9,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := ims_shimmy.cpp
-LOCAL_MULTILIB := 32
+LOCAL_MULTILIB := 64
 LOCAL_MODULE := libims_shim
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
