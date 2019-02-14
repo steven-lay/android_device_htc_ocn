14e99165b1db332ad86c41d9d133b09323bf1e16
tomascus <arbiter1000@gmail.com>
2019-02-14 20:38:14 +1100

    Partially revert "ocn: remove some unneeded graphics props"
    This reverts commit 9d6a3201ac63364f7a46a44f5b8ae4b5ae563056.
    
    Change-Id: I950142bdce895c995555eab1bfc1e9067c62798a

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -87,11 +87,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
+    debug.sf.latch_unsignaled=1 \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
     ro.opengles.version=196610 \
-    ro.sf.lcd_density=640
+    ro.sf.lcd_density=640 \
+    vendor.display.disable_rotator_downscale=1 \
+    vendor.gralloc.enable_fb_ubwc=1 \
+    vendor.display.disable_skip_validate=1
 
 # Factory Reset Protection
 PRODUCT_PROPERTY_OVERRIDES += \
