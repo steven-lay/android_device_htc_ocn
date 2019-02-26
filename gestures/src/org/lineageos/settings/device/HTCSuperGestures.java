269f5e30a0faa88386671bad9bb786979f1f3b85
tomascus <arbiter1000@gmail.com>
2019-02-26 22:24:20 +1100

    ocn: gestures: create superclass out of classes
    Reduce the massive amounts of code duplication we have here.
    
    Change-Id: If4b21259182193d12b26778139b2fe3864c1c5e7

---
+++ gestures/src/org/lineageos/settings/device/HTCSuperGestures.java
@@ -0,0 +1,264 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.device;
+
+import android.app.Service;
+import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraAccessException;
+import android.Manifest;
+import android.media.AudioManager;
+import android.media.session.MediaSessionLegacyHelper;
+import android.net.Uri;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.os.SystemClock;
+import android.os.UserHandle;
+import android.os.Vibrator;
+import android.os.VibrationEffect;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.view.KeyEvent;
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.app.Instrumentation;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ApplicationInfo;
+import android.view.MotionEvent;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.graphics.PixelFormat;
+import android.view.Display;
+import android.view.Gravity;
+import android.os.Handler;
+
+import java.util.Iterator;
+import java.lang.Runtime;
+import java.io.IOException;
+
+import lineageos.providers.LineageSettings;
+import org.lineageos.internal.util.FileUtils;
+
+import java.util.List;
+
+public abstract class HTCSuperGestures extends Service {
+
+    private static final String GESTURE_WAKEUP_REASON = "gesture-wakeup";
+    protected static final String TAG = "GestureService";
+
+    private AudioManager mAudioManager;
+    private CameraManager mCameraManager;
+    private String mRearCameraId;
+    private Vibrator mVibrator;
+    private boolean mTorchEnabled;
+    protected Context mContext;
+    protected PowerManager mPowerManager;
+    protected WakeLock mGestureWakeLock;
+    protected boolean mHapticIgnoreRinger;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mContext = this;
+
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        mCameraManager.registerTorchCallback(new TorchModeCallback(), null);
+        mPowerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        mGestureWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "HtcGestureWakeLock");
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    protected class TorchModeCallback extends CameraManager.TorchCallback {
+        @Override
+        public void onTorchModeChanged(String cameraId, boolean enabled) {
+            if (!cameraId.equals(mRearCameraId)) return;
+            mTorchEnabled = enabled;
+        }
+
+        @Override
+        public void onTorchModeUnavailable(String cameraId) {
+            if (!cameraId.equals(mRearCameraId)) return;
+            mTorchEnabled = false;
+        }
+    }
+
+    protected void launchCamera() {
+        final Intent intent = new Intent(lineageos.content.Intent.ACTION_SCREEN_CAMERA_GESTURE);
+        mContext.sendBroadcastAsUser(intent, UserHandle.CURRENT,
+                Manifest.permission.STATUS_BAR_SERVICE);
+    }
+
+    protected void launchBrowser() {
+        mPowerManager.wakeUp(SystemClock.uptimeMillis(), GESTURE_WAKEUP_REASON);
+        final Intent intent = getLaunchableIntent(
+                new Intent(Intent.ACTION_VIEW, Uri.parse("http:")));
+        startActivitySafely(intent);
+    }
+
+    protected void launchDialer() {
+        mPowerManager.wakeUp(SystemClock.uptimeMillis(), GESTURE_WAKEUP_REASON);
+        final Intent intent = new Intent(Intent.ACTION_DIAL, null);
+        startActivitySafely(intent);
+    }
+
+    protected void launchEmail() {
+        mPowerManager.wakeUp(SystemClock.uptimeMillis(), GESTURE_WAKEUP_REASON);
+        final Intent intent = getLaunchableIntent(
+                new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:")));
+        startActivitySafely(intent);
+    }
+
+    protected void launchMessages() {
+        mPowerManager.wakeUp(SystemClock.uptimeMillis(), GESTURE_WAKEUP_REASON);
+        final String defaultApplication = Settings.Secure.getString(
+                mContext.getContentResolver(), "sms_default_application");
+        final PackageManager pm = mContext.getPackageManager();
+        final Intent intent = pm.getLaunchIntentForPackage(defaultApplication);
+        if (intent != null) {
+            startActivitySafely(intent);
+        }
+    }
+
+    protected void toggleFlashlight() {
+        String rearCameraId = getRearCameraId();
+        if (rearCameraId != null) {
+            try {
+                mCameraManager.setTorchMode(rearCameraId, !mTorchEnabled);
+                mTorchEnabled = !mTorchEnabled;
+            } catch (CameraAccessException e) {
+                // Ignore
+            }
+        }
+    }
+
+    protected void playPauseMusic() {
+        dispatchMediaKeyWithWakeLockToMediaSession(KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
+    }
+
+    protected void previousTrack() {
+        dispatchMediaKeyWithWakeLockToMediaSession(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
+    }
+
+    protected void nextTrack() {
+        dispatchMediaKeyWithWakeLockToMediaSession(KeyEvent.KEYCODE_MEDIA_NEXT);
+    }
+
+    protected void volumeDown() {
+        mAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, 0);
+    }
+
+    protected void volumeUp() {
+        mAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_RAISE, 0);
+    }
+
+    protected void dispatchMediaKeyWithWakeLockToMediaSession(final int keycode) {
+        final MediaSessionLegacyHelper helper = MediaSessionLegacyHelper.getHelper(mContext);
+        if (helper == null) {
+            Log.w(TAG, "Unable to send media key event");
+            return;
+        }
+        KeyEvent event = new KeyEvent(SystemClock.uptimeMillis(),
+                SystemClock.uptimeMillis(), KeyEvent.ACTION_DOWN, keycode, 0);
+        helper.sendMediaButtonEvent(event, true);
+        event = KeyEvent.changeAction(event, KeyEvent.ACTION_UP);
+        helper.sendMediaButtonEvent(event, true);
+    }
+
+    protected void startActivitySafely(final Intent intent) {
+        if (intent == null) {
+            Log.w(TAG, "No intent passed to startActivitySafely");
+            return;
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP
+                | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        try {
+            final UserHandle user = new UserHandle(UserHandle.USER_CURRENT);
+            mContext.startActivityAsUser(intent, null, user);
+        } catch (ActivityNotFoundException e) {
+            // Ignore
+        }
+    }
+
+    protected void doHapticFeedback() {
+        final boolean enabled = LineageSettings.System.getInt(mContext.getContentResolver(),
+                    LineageSettings.System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK, 1) != 0;
+        if (mVibrator == null) {
+            return;
+        }
+        mVibrator.vibrate(50);
+    }
+
+    protected String getRearCameraId() {
+        if (mRearCameraId == null) {
+            try {
+                for (final String cameraId : mCameraManager.getCameraIdList()) {
+                    final CameraCharacteristics characteristics =
+                            mCameraManager.getCameraCharacteristics(cameraId);
+                    final int orientation = characteristics.get(CameraCharacteristics.LENS_FACING);
+                    if (orientation == CameraCharacteristics.LENS_FACING_BACK) {
+                        mRearCameraId = cameraId;
+                        break;
+                    }
+                }
+            } catch (CameraAccessException e) {
+                // Ignore
+            }
+        }
+        return mRearCameraId;
+    }
+
+    protected Intent getLaunchableIntent(Intent intent) {
+        PackageManager pm = mContext.getPackageManager();
+        List<ResolveInfo> resInfo = pm.queryIntentActivities(intent, 0);
+        if (resInfo.isEmpty()) {
+            return null;
+        }
+        return pm.getLaunchIntentForPackage(resInfo.get(0).activityInfo.packageName);
+    }
+
+
+}
