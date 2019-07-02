fbdc3deec58b713091b54b7e3d3db38668bc1ec8
tomascus <arbiter1000@gmail.com>
2019-07-02 17:36:24 +1000

    Revert "ocn: kang doze package from xiaomi msm8998"
    This reverts commit 455d61cdd0092e42d9c3150704ba2481f6e71034.

--- device.mk
+++ device.mk
@@ -183,10 +183,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     qdcm_calib_data_WQHD_JDI_S6D1HA2_5.5_command_mode_panel.xml
-    
-# Doze
-PRODUCT_PACKAGES += \
-    HTCDoze
 
 # DRM
 PRODUCT_PACKAGES += \
