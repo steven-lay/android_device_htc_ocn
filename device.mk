12bf14126dc0d6f07d1f3067afeb738f3653ccf0
tomascus <arbiter1000@gmail.com>
2019-09-07 19:35:40 +1000

    ocn: add whitelist perms and move to system
    Change-Id: Ib53324dbaf9283e8df640dd1de383cb9274aa9c3

--- device.mk
+++ device.mk
@@ -276,8 +276,8 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # Keymaster
 PRODUCT_PACKAGES += \
